f3fc74f0b9398f4f0d5a9fe213db251530320c65
Jaiditya Yadav <jaiditya2003@gmail.com>
2025-10-05 21:15:54 +0530

    working compose and inbound rules updates

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,37 +23,13 @@ resource "aws_security_group" "videocopilot_sg" {
   description = "Security group for VideoCopilot application"
   vpc_id      = data.aws_vpc.default.id
 
-  # SSH access from anywhere
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  # HTTP access from anywhere
+  # Allow all inbound traffic (all ports, all protocols)
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS access from anywhere
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
+    description = "Allow all inbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
